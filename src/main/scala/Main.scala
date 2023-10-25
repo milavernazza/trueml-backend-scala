50f79155c41af2164d2d11befa96fd97f9189825
Mila Vernazza <milavernazza@gmail.com>
2023-10-25 01:26:41 -0300

    Implemented main application entry point with GraphQL route setup

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,39 +1,24 @@
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
-import sangria.execution.Executor
-import sangria.parser.QueryParser
-import scala.concurrent.ExecutionContext
+import api.routes.GraphQLRoutes
+import domain.services.{CustomerService, CustomerServiceImpl}
 import scala.io.StdIn
 
-object Main extends App {
+object Main extends App with GraphQLRoutes {
+  
   implicit val system: ActorSystem = ActorSystem("trueml-backend-scala")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
-  implicit val executionContext: ExecutionContext = system.dispatcher
+  
+  override val customerService: CustomerService = new CustomerServiceImpl()
 
-  val customerService = new CustomerServiceImpl()
-
-    val route: Route = path("graphql") {
-    post {
-      entity(as[JsValue]) { requestJson =>
-        val JsObject(fields) = requestJson
-        val JsString(query) = fields("query")
-        val operation = fields.get("operationName").collect {
-          case JsString(op) => op
-        }
-        val vars = fields.get("variables").getOrElse(JsObject.empty).asJsObject
-        
-        GraphQLExecutor.executeQuery(query, operation, vars)(customerService)
-      }
-    }
-  }
+  val route = graphqlRoute
 
   val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 8080)
 
   println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-  StdIn.readLine()
+  StdIn.readLine() // let it run until user presses return
   bindingFuture
-    .flatMap(_.unbind())
-    .onComplete(_ => system.terminate())
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done
 }
