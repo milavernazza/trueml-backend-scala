52593a20595c33efdcce4cbf8ac9b7b64e9cb955
Mila Vernazza <milavernazza@gmail.com>
2023-10-25 01:28:25 -0300

    Implemented integration tests for GraphQL endpoint

--- src/main/scala/domain/services/CustomerServiceSpec.scala
+++ src/main/scala/domain/services/CustomerServiceSpec.scala
@@ -1,11 +1,26 @@
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import api.routes.GraphQLRoutes
+import domain.services.{CustomerService, CustomerServiceImpl}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class CustomerServiceSpec extends AnyFlatSpec with Matchers {
-  "A CustomerService" should "be able to create and retrieve customers" in {
-    val service = new CustomerService
-    val customer = Customer("1", "John", "Doe", "john.doe@example.com")
-    service.createCustomer(customer)
-    service.getAllCustomers should contain (customer)
+class GraphQLSpec extends AnyFlatSpec with Matchers with ScalatestRouteTest with GraphQLRoutes {
+
+  override val customerService: CustomerService = new CustomerServiceImpl()
+
+  "The GraphQL endpoint" should "handle queries and mutations" in {
+
+    // Test query
+    Post("/graphql", """{"query": "{ customers { id, firstName } }"}""") ~> graphqlRoute ~> check {
+      status shouldEqual StatusCodes.OK
+      // ... other checks
+    }
+
+    // Test mutation
+    Post("/graphql", """{"query": "mutation { createCustomer(id: \"1\", firstName: \"John\", lastName: \"Doe\", email: \"john.doe@example.com\") { id, firstName } }"}""") ~> graphqlRoute ~> check {
+      status shouldEqual StatusCodes.OK
+      // ... other checks
+    }
   }
 }
